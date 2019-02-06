8accf98f14efbeda303b246b74f6aa47e014fbac
glaze <i@inevitable.tech>
2019-02-05 21:20:56 -0800

    place to show the notes

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-var instance = axios.create({
-        baseURL: 'https://pastebinnn.azurewebsites.net/api/',
-        timeout: 1000,
-        headers: {'X-Custom-Header': 'foobar','Access-Control-Allow-Origin':'*'}
-      });
+var notesCard = document.createElement("div.mdui-card.mdui-hoverable")
+var notesContainer = document.createElement("p")
 
 
 function queryFornotes(){
     
     let username=document.getElementById('username').value
     // console.log(username)
-    axios.get('https://pastebinnn.azurewebsites.net/api/ReadFromUserName?name=cruz',instance)
+    axios.get('https://pastebinnn.azurewebsites.net/api/ReadFromUserName?name=cruz')
   .then(function (response) {
-    console.log(response);
+   let notes=JSON.parse(response.data)
+    notes.forEach(element => {
+      let notesCard = document.createElement("div.mdui-card.mdui-hoverable")
+      let notesContainer = document.createElement("p")
+      let notesText= document.createTextNode(element.Notes)
+      notesContainer.appendChild(notesText)
+      notesCard.appendChild(notesContainer)
+      let el=Document.getElementById('notsPosition')
+      el.appendChild(notesCard)
+    });
   })
   .catch(function (error) {
     console.log(error);
