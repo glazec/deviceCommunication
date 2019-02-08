41fa31f0491e2c6e4fca4f5150ae260ddb3fb92c
glaze <i@inevitable.tech>
2019-02-08 13:02:29 -0800

    fix notes display css error

--- index.js
+++ index.js
@@ -5,27 +5,33 @@ var notesContainer = document.createElement("p")
 function queryFornotes(){
     
     let username=document.getElementById('username').value
-    // console.log(username)
-    // let url="http://localhost:7071/api/ReadFromUserName?name=cruz"
     let url="https://pastebinnn.azurewebsites.net/api/ReadFromUserName?name=cruz"
     axios.get(url)
   .then(function (response) {
-    console.log(response)
-    let swap=JSON.parse(response.data)
-   let notes=swap
-   console.log(notes)
-    notes.forEach(element => {
-      let notesCard = document.createElement("div.mdui-card.mdui-hoverable")
-      let notesContainer = document.createElement("p")
-      let notesText= document.createTextNode(element.Notes)
-      notesContainer.appendChild(notesText)
-      notesCard.appendChild(notesContainer)
-      let el=Document.getElementById('notsPosition')
-      el.appendChild(notesCard)
+    try {
+
+      response.data.forEach(element => {
+      notesDisplayCard(element.Notes)
     });
+
+    } catch (error) {
+      notesDisplayCard(response.data.Notes)
+    }
+    
   })
   .catch(function (error) {
     console.log(error);
   });
 
+}
+
+function notesDisplayCard(notes){
+  let notesCard = document.createElement("div")
+  notesCard.className='mdui-card mdui-hoverable'
+  let notesContainer = document.createElement("p")
+  let notesText= document.createTextNode(notes)
+  notesContainer.appendChild(notesText)
+  notesCard.appendChild(notesContainer)
+  let el=document.getElementById('notsPosition')
+  el.appendChild(notesCard)
 }
