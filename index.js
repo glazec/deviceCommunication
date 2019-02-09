741c8bddeb05c8bb0340c1a360bd22d36c8d6b95
glaze <i@inevitable.tech>
2019-02-08 22:11:31 -0800

    upload file

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+function upload(value){
+  let username=value
+  let file=document.getElementById("inputFile")
+  let notes=document.getElementById('notes').value
+  var instance = axios.create({
+    timeout: 1000,
+    headers: {'Content-Type': 'application/json'},
+    method: 'post',
+    params:{
+      file: file,
+      model: 1,
+      action: upload
+    }
+  });
+  
+  if (file){
+    axios.post('http://tmp.link/openapi/v1', instance)
+    .then(function (response) {
+      console.log(response);
+      saveGist(username,response.data.ukey)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+  if(notes){
+    saveGist(username,notes)
+  }
+
+}
+
 function queryFornotes(){
     clearNotesCard();
     let username=document.getElementById('username').value
@@ -50,7 +81,7 @@ function clearNotesCard(){
 function NotesInputConfirm(){
   mdui.prompt('Username',
     function (value) {
-      saveGist(value)
+      upload(value)
     },
     function (value) {
     }
@@ -58,9 +89,8 @@ function NotesInputConfirm(){
 }
 
 
-function  saveGist(username){
-  let notes=document.getElementById('notes').value
-  let url="https://pastebinnn.azurewebsites.net/api/saveGist?name="+username+"&notes="+notes
+function  saveGist(username,data){
+  let url="https://pastebinnn.azurewebsites.net/api/saveGist?name="+username+"&notes="+data
     axios.get(url)
   .then(function (response) {
       console.log(response.data)
